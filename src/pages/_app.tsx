5d8c6a9291466626bb1e6117f8736b64d6b65906
yunsu <than4502@gmail.com>
2023-05-24 18:03:51 +0900

    refact: app init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,44 +1,29 @@
 import Head from "next/head";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 import "@/styles/globals.css";
 import nearStore from "@/store/nearStore";
-import { getWalletAuthKey } from "@/utils/auth";
 
 const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_NAME;
 
 const App = ({ Component, pageProps }: AppProps) => {
   const { init, setIsWalletStarted, setFtBalance } = nearStore();
 
-  const viewFtToken = async (wallet: any) => {
-    const authKey = getWalletAuthKey();
-    if (!authKey) return;
-
-    return wallet.viewMethod({
-      contractId: CONTRACT_ADDRESS,
-      method: "ft_balance_of",
-      args: {
-        account_id: getWalletAuthKey(),
-      },
-    });
-  };
-
-  useEffect(() => {
+  const initApp = useCallback(async () => {
     const wallet = init(CONTRACT_ADDRESS);
 
-    (async () => {
-      try {
-        await wallet?.startUp();
-        setIsWalletStarted(true);
+    try {
+      await wallet?.startUp();
+      setIsWalletStarted(true);
+    } catch (e) {
+      console.error(e);
+    }
+  }, [init, setIsWalletStarted]);
 
-        const ftBalance = await viewFtToken(wallet);
-        setFtBalance(ftBalance);
-      } catch (e) {
-        console.error(e);
-      }
-    })();
-  }, [init, setFtBalance, setIsWalletStarted]);
+  useEffect(() => {
+    initApp();
+  }, [initApp]);
 
   return (
     <>
@@ -64,6 +49,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <meta name="og:type" content="website" />
         <meta name="og:image" content="/svgs/next-logo.svg" />
       </Head>
+
       <Component {...pageProps} />
     </>
   );
