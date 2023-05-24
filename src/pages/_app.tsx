1097404cc21b1b6768aef33066604eeaddf5b180
yunsu <than4502@gmail.com>
2023-05-24 15:56:53 +0900

    feat: meta data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 
@@ -39,7 +40,33 @@ const App = ({ Component, pageProps }: AppProps) => {
     })();
   }, [init, setFtBalance, setIsWalletStarted]);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Whats Next</title>
+        <meta
+          name="description"
+          content="AI Vote Toon App with Near Protocol"
+        />
+        <link rel="icon" href="/svgs/next-logo.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="keywords"
+          content="Web3, AI-Image, Vote, Near, Coummunity"
+        />
+        <meta name="og:site_name" content="Whats Next" />
+        <meta name="og:title" content="AI Toon App" />
+        <meta
+          name="og:description"
+          content="A webtoon community that combines AI image generation and a voting system using near protocol"
+        />
+        <meta name="og:url" content="https://whats-next-demo.vercel.app" />
+        <meta name="og:type" content="website" />
+        <meta name="og:image" content="/svgs/next-logo.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
